c0f9732b459f8988b827f52a8157cca5ba12ee76
Polina Mitrikovskaia <pmitrikovskaia@google.com>
2025-12-17 13:30:33 -0800

    [FIR] Add receiver type to UNRESOLVED_REFERENCE error message if present
    This helps the user know the type they are acting on, which would make
    it more obvious the user is acting on the wrong type.
    Also update frontend and autogenerated tests for UNRESOLVED_REFERENCE
    error message
    
    ^KT-73431 Fixed

--- analysis/analysis-api-fir/gen/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDataClassConverters.kt
+++ analysis/analysis-api-fir/gen/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDataClassConverters.kt
@@ -4504,6 +4504,7 @@ private fun KaDiagnosticConverterBuilder.addConversions102() {
         UnresolvedReferenceImpl(
             firDiagnostic.a,
             firDiagnostic.b,
+            firDiagnostic.c?.let { firSymbolBuilder.typeBuilder.buildKtType(it) },
             firDiagnostic as KtPsiDiagnostic,
             token,
         )

9a9aa68847dad5624df84b2587ea05e56d4b9331
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-01-07 15:16:38 +0100

    ~ WIP basic proj struct

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -165,6 +165,7 @@ include(
     ":compiler:plugin-api",
     ":compiler:light-classes",
     ":compiler:javac-wrapper",
+    ":compiler:java-direct",
     ":compiler:cli:cli-arguments-generator",
     ":compiler:cli-base",
     ":compiler:cli",

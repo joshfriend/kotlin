8cf4768f0dd2d16c5e77e6dad559fcd2f7e6bff9
Aleksandr.Shefer <aleksandr.shefer@jetbrains.com>
2026-03-24 12:16:20 +0100

    [Wasm] introduce optional WasmSuspendFunctionsLowering

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
@@ -187,6 +187,7 @@ val wasmLowerings: List<NamedCompilerPhase<WasmBackendContext, IrModuleFragment,
     ::EnumUsageLowering,
     ::EnumClassRemoveEntriesLowering,
 
+    ::WasmSuspendFunctionsLowering,
     ::WasmInitializersLowering,
     ::WasmInitializersCleanupLowering,
 

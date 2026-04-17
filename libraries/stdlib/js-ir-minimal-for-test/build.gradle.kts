e7f4810ba78a6bd05b2a8cea90b5a95a8bee8fcd
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-04-17 19:27:29 +0200

    [JS] Use `JsCodegenPipelinePhase` from tests
    This makes the corresponding pipeline phase the single entry point
    for JS code generation that can be used from both production compiler
    and the test infrastructure instead of calling arbitrary functions from
    compiler internals.
    
    KT-77029

--- libraries/stdlib/js-ir-minimal-for-test/build.gradle.kts
+++ libraries/stdlib/js-ir-minimal-for-test/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
@@ -9,6 +8,11 @@ plugins {
 kotlin {
     js {
         nodejs()
+
+        // Use the same name as the full stdlib. This is so that in per-module box tests, the JS module corresponding
+        // to the standard library would have a predicatable name, no matter which flavor of stdlib the test is compiled against.
+        // In some test logic, there are certain assumptions about that name. For example, see `JsWrongModuleHandler`.
+        outputModuleName.set("kotlin-kotlin-stdlib")
     }
 }
 val commonMainFullSources by task<Sync> {

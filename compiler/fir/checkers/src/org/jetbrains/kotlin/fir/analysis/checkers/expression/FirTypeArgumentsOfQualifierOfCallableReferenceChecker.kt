2d59382d840cb75f6d9c566b97d6e02af757d798
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-04-23 20:13:29 +0300

    [FIR] Minor: Extract logic to local function

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirTypeArgumentsOfQualifierOfCallableReferenceChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirTypeArgumentsOfQualifierOfCallableReferenceChecker.kt
@@ -19,11 +19,13 @@ import org.jetbrains.kotlin.fir.analysis.checkers.extractArgumentsTypeRefAndSour
 import org.jetbrains.kotlin.fir.analysis.checkers.toTypeArgumentsWithSourceInfo
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.declarations.utils.isLocal
+import org.jetbrains.kotlin.fir.diagnostics.ConeDiagnostic
 import org.jetbrains.kotlin.fir.expressions.FirCallableReferenceAccess
 import org.jetbrains.kotlin.fir.expressions.FirResolvedQualifier
 import org.jetbrains.kotlin.fir.expressions.unwrapSmartcastExpression
 import org.jetbrains.kotlin.fir.isEnabled
 import org.jetbrains.kotlin.fir.resolve.classTypeParameterSymbols
+import org.jetbrains.kotlin.fir.resolve.diagnostics.ConeDiagnosticInCallableReferenceLhs
 import org.jetbrains.kotlin.fir.resolve.diagnostics.ConeOuterClassArgumentsRequired
 import org.jetbrains.kotlin.fir.resolve.diagnostics.ConePlaceholderProjectionInQualifierResolution
 import org.jetbrains.kotlin.fir.resolve.diagnostics.ConeWrongNumberOfTypeArgumentsError
@@ -52,8 +54,9 @@ object FirTypeArgumentsOfQualifierOfCallableReferenceChecker : FirCallableRefere
         classSymbol: FirClassLikeSymbol<*>,
         lhsType: ConeKotlinType,
     ): Severity? {
-        var warningWasReported = false
-        for (diagnostic in expression.nonFatalDiagnostics) {
+        var severity: Severity? = null
+
+        fun checkSingleAndUpdateSeverity(diagnostic: ConeDiagnostic) {
             when (diagnostic) {
                 is ConeWrongNumberOfTypeArgumentsError -> {
                     val positioning =
@@ -84,18 +87,17 @@ object FirTypeArgumentsOfQualifierOfCallableReferenceChecker : FirCallableRefere
                                 positioningStrategy = positioning,
                             )
                         }
-                        warningWasReported = true
-                        continue
+                        severity = severity ?: Severity.WARNING
+                    } else {
+                        reporter.reportOn(
+                            diagnostic.source,
+                            FirErrors.WRONG_NUMBER_OF_TYPE_ARGUMENTS,
+                            diagnostic.desiredCount,
+                            diagnostic.symbol,
+                            positioningStrategy = positioning,
+                        )
+                        severity = Severity.ERROR
                     }
-
-                    reporter.reportOn(
-                        diagnostic.source,
-                        FirErrors.WRONG_NUMBER_OF_TYPE_ARGUMENTS,
-                        diagnostic.desiredCount,
-                        diagnostic.symbol,
-                        positioningStrategy = positioning,
-                    )
-                    return Severity.ERROR
                 }
                 is ConeOuterClassArgumentsRequired -> {
                     reporter.reportOn(
@@ -103,19 +105,23 @@ object FirTypeArgumentsOfQualifierOfCallableReferenceChecker : FirCallableRefere
                         FirErrors.OUTER_CLASS_ARGUMENTS_REQUIRED,
                         diagnostic.symbol,
                     )
-                    return Severity.ERROR
+                    severity = Severity.ERROR
+                }
+                else -> {
                 }
             }
         }
-        return Severity.WARNING.takeIf { warningWasReported }
+
+        for (diagnostic in expression.nonFatalDiagnostics) {
+            checkSingleAndUpdateSeverity(diagnostic)
+        }
+        return severity
     }
 
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(expression: FirCallableReferenceAccess) {
         val lhs = expression.explicitReceiver?.unwrapSmartcastExpression() as? FirResolvedQualifier ?: return
         val correspondingDeclaration = lhs.symbol ?: return
-        val lhsType = lhs.resolvedLhsTypeForCallableReferenceOrNull ?: return
-
         for (argument in lhs.typeArguments) {
             val errorTypeRef = (argument as? FirTypeProjectionWithVariance)?.typeRef as? FirErrorTypeRef ?: continue
 
@@ -123,6 +129,7 @@ object FirTypeArgumentsOfQualifierOfCallableReferenceChecker : FirCallableRefere
                 reporter.reportOn(argument.source, FirErrors.PLACEHOLDER_PROJECTION_IN_QUALIFIER)
             }
         }
+        val lhsType = lhs.resolvedLhsTypeForCallableReferenceOrNull ?: return
 
         val nonFatalDiagnosticsCheckResult = checkNonFatalDiagnostics(expression, lhs, correspondingDeclaration, lhsType)
         if (nonFatalDiagnosticsCheckResult == Severity.ERROR) return

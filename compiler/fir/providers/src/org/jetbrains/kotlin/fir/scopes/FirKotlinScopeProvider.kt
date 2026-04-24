36bd07288c14d2acb54f7ae0efc8d765c452587c
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-04-24 15:03:37 +0200

    [FIR] provide type alias constructors for delegated constructor calls
    ^KT-85989 Fixed

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/FirKotlinScopeProvider.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/FirKotlinScopeProvider.kt
@@ -289,6 +289,26 @@ fun FirTypeAlias.scopeForTypeAlias(
     return scopeProvider.getTypealiasConstructorScope(this, useSiteSession, scopeSession)
 }
 
+context(c: SessionAndScopeSessionHolder)
+fun FirClassLikeDeclaration.scopeForConstructors(
+    substitutor: ConeSubstitutor,
+    memberOwnerClass: FirClassSymbol<*>?,
+): FirScope? {
+    val scope = when (this) {
+        is FirTypeAlias -> scopeForTypeAlias(c.session, c.scopeSession)
+        is FirClass -> when (classKind) {
+            ClassKind.INTERFACE -> null
+            else -> scopeForClass(
+                substitutor,
+                memberOwnerClass = memberOwnerClass ?: symbol,
+                memberRequiredPhase = FirResolvePhase.STATUS,
+            )
+        }
+    }
+
+    return scope
+}
+
 fun ConeKotlinType.scopeForSupertype(
     useSiteSession: FirSession,
     scopeSession: ScopeSession,
